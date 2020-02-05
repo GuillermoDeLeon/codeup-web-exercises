59069f5383cd7e5c58a28c97acdbb5094f247ed1
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-02-05 11:17:24 -0600

    making updates

--- js/external.js
+++ js/external.js
@@ -2,6 +2,8 @@ console.log("Hello from external JavaScript");
 
 // alert('Welcome to my Website!');
 
+
+
 //Exercise 3.1
 
 // var userInput = prompt('What is your favorite color?');
@@ -37,8 +39,57 @@ console.log("Hello from external JavaScript");
 
 //Exercise 3.3
 
-var isClassFull = false;
-var scheduleConflict = false;
+// var isClassFull;
+// var scheduleConflict;
+//
+// var responseScheduleConflict = prompt('Does the class you want to sign up for take place at the same time as any of your current classes?  1 - yes; 2 - no');
+// var responseisClassFull = prompt('Is the class you are wanting to take already full? 1 - yes; 2 - no');
+//
+// console.log(responseScheduleConflict);
 
-var responseScheduleConflict = prompt('Does the class you want to sign up for take place at the same time as any of your current classes?');
-var responseisClassFull = prompt('Is the class you are wanting to take already full?');
+
+
+// if (responseScheduleConflict = 'yes') {
+//     scheduleConflict = true; }
+// else {
+//     scheduleConflict = false; }
+//
+// if (responseisClassFull = 'yes') {
+//     isClassFull = true; }
+// else {
+//     isClassFull = false; }
+//
+// if (isClassFull || scheduleConflict = true) {
+//     alert("I am sorry, but either the class you want to take is full or conflicts with your current schedule.");
+// }
+// else {
+//     alert("Great! The class is not full and there are no scheduling conflicts");
+// }
+
+var classIsNotFull = confirm("Class is not full");
+var noScheduleConflicts = confirm("Schedules do not conflict");
+
+var studentEnrolled = classIsNotFull && noScheduleConflicts;
+alert("Student can enroll: " + studentEnrolled);
+
+
+// Exercise 3.4
+
+// var itemsPurchased;
+// var isOfferExpired;
+// var isPremiumMember;
+//
+// itemsPurchased = Number(prompt('How many items did you purchase?'));
+// isOfferExpired = Number(prompt('Is your offer expired? 0 - no; 1 - yes'));
+// isPremiumMember = Number(prompt('Are you a premium member? 0 - no; 1 - yes'));
+//
+// console.log(itemsPurchased);
+// console.log(isOfferExpired);
+// console.log(isPremiumMember);
+//
+// if ((isPremiumMember == 1 || itemsPurchased > 2) && isOfferExpired == 0) {
+//     alert("You are either a Premium member or have purchased more than 2 products; You have an offer that is not expired");
+// }
+// else {
+//     alert("You either have not purchased more than 2 items or are not a Premium member; You may also have an EXPIRED offer.");
+// }
