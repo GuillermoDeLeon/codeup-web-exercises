5d26764f6cdf78553188d7ff0eda54c6bdb95c72
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-02-11 22:51:49 -0600

    loop exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -14,6 +14,23 @@
 
 //Loop Exercise 3
 
-for (var i=1; i <= 10; i++) {
+// for (var i=1; i <= 10; i++) {
+//     var random = Math.floor(Math.random() * 180) + 20;
+//     if (random % 2 === 1) {
+//         console.log(random + " is odd");
+//     } else {
+//         console.log(random + " is even");
+//     }
+// }
 
-}
+//Loop Exercise 4
+
+// for (var i=1; i < 10; i++) {
+//     console.log(i);
+// }
+
+//Loop Exercise 5
+
+// for (var i=100; i > 0; i-=5) {
+//     console.log(i);
+// }
