3fee7ce8cfb4afdefd92a33ff28d89c1b0d21f41
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-02-26 21:30:44 -0600

    object drills aphorisms and other various commits

---
+++ js/my-custom-javascript.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(document).ready(function() {
+
+    alert($('h1').html());
+// alert('this page is loading, from js file')
+    // INSERT JAVASCRIPT CODE SAMPLE HERE
+    $('#div1').css('background-color', '#FF0');
+    $('#div2').css('background-color', 'blue');
+
+    alert($('h4').html());
+
+});
