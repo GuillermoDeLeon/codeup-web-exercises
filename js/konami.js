24c93a2e30bf86d22a4b6c43196fadcc71cd387b
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-02-28 02:46:29 -0600

    completed basic functions of konami code

--- js/konami.js
+++ js/konami.js
@@ -1,48 +1,58 @@
 "use strict";
 
-$(document).ready(function() {  //beginning of JQuery ready
-    var kcodeArr = 'u u d d l r l r b a';
-    var codeArray = [];
+$(document).ready(function() {  //beginning of document ready
+    var konamiCodeArray = ['u','u','d','d','l','r','l','r','b','a','enter'];
+    var bucketArray = [];
+    var ctr = 0;
 
-    $('html').keydown(function(e) { //this will capture any 'keydown' event on any part of the browser page - in this case it will be specific to capturing arrow inputs
-        var arrowDirection = e.which;
-        console.log(arrowDirection);
+    $('html').keydown(function(e) { //this will capture any 'keydown' event on any part of the browser page
+        var userInput = e.which;
+        console.log(userInput);
 
-        switch(arrowDirection) {
+        switch(userInput) {
             case 37:
                 $("#key-pressed").html("left arrow");
-                codeArray.push("l");
+                bucketArray.push("l");
                 break;
             case 38:
                 $("#key-pressed").html("up arrow");
-                codeArray.push("u");
+                bucketArray.push("u");
                 break;
             case 39:
                 $("#key-pressed").html("right arrow");
-                codeArray.push("r");
+                bucketArray.push("r");
                 break;
             case 40:
                 $("#key-pressed").html("down arrow");
-                codeArray.push("d");
+                bucketArray.push("d");
                 break;
             case 66:
                 $("#key-pressed").html("B");
-                codeArray.push("b");
+                bucketArray.push("b");
                 break;
             case 65:
                 $("#key-pressed").html("A");
-                codeArray.push("a");
+                bucketArray.push("a");
                 break;
             case 13:
                 $("#key-pressed").html("enter");
-                codeArray.push("enter");
+                bucketArray.push("enter");
                 break;
             default:
                 $("#key-pressed").html("This is not a valid input!");
         }
-        $("#code-input").html(codeArray);
-        console.log(codeArray.join(','));
+    //stores valid keys pressed by user in an array and also displays the stored keystrokes on the browser page using JQuery
+        $("#keys-captured").html(bucketArray);
 
+    //will compare the keys pressed by user with the actual Konami code.  If the code entered by user matches the correct Konami code, then alert will launch.
+        if (bucketArray.length === konamiCodeArray.length) {
+            if(JSON.stringify(bucketArray) === JSON.stringify(konamiCodeArray)) {
+                alert('You have added 30 lives!');
+                return console.log(true);
+            } else {
+                return console.log(false);
+            }
+        }
+        ctr++;
     });
-
-});  // end of JQuery ready
+});  // end of document ready
