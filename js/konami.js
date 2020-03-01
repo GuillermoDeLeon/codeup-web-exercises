0d1295b6050cd16a525a7f0474ab8ecbd7be87fa
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-03-01 17:23:57 -0600

    more konami updates

--- js/konami.js
+++ js/konami.js
@@ -5,12 +5,10 @@ $(document).ready(function() {  //beginning of document ready
     var bucketArray = [];
     var ctr = 0;
 
-
     $("button").click(function(){
         var imageUrl = "img/konami/contra-background.png";
         $(".box").css("background-image", "url(" + imageUrl + ")");
     });
-
 
     $('html').keyup(function(e) { //this will capture any 'keyup' event on any part of the browser page
         var userInput = e.which;
@@ -18,39 +16,38 @@ $(document).ready(function() {  //beginning of document ready
 
         switch(userInput) {
             case 37:
-                $("#key-pressed").html("left").prepend($('<img>',{id:'#key-pressed',src:'img/konami/left-arrow.png'}));
+                $("#key-pressed").html("").prepend($('<img>',{id:'#key-pressed',src:'img/konami/left-arrow.png'}));
                 bucketArray.push("l");
                 break;
             case 38:
-                $("#key-pressed").html("up arrow").prepend($('<img>',{id:'#key-pressed',src:'img/konami/up-arrow.png'}));
+                $("#key-pressed").html("").prepend($('<img>',{id:'#key-pressed',src:'img/konami/up-arrow.png'}));
                 bucketArray.push("u");
                 break;
             case 39:
-                $("#key-pressed").html("right arrow").prepend($('<img>',{id:'#key-pressed',src:'img/konami/right-arrow.png'}));
+                $("#key-pressed").html("").prepend($('<img>',{id:'#key-pressed',src:'img/konami/right-arrow.png'}));
                 bucketArray.push("r");
                 break;
             case 40:
-                $("#key-pressed").html("down arrow").prepend($('<img>',{id:'#key-pressed',src:'img/konami/down-arrow.png'}));
+                $("#key-pressed").html("").prepend($('<img>',{id:'#key-pressed',src:'img/konami/down-arrow.png'}));
                 bucketArray.push("d");
                 break;
             case 66:
-                $("#key-pressed").html("B").prepend($('<img>',{id:'#key-pressed',src:'img/konami/letter-b.png'}));
+                $("#key-pressed").html("").prepend($('<img>',{id:'#key-pressed',src:'img/konami/letter-b.png'}));
                 bucketArray.push("b");
                 break;
             case 65:
-                $("#key-pressed").html("A").prepend($('<img>',{id:'#key-pressed',src:'img/konami/letter-a.png'}));
+                $("#key-pressed").html("").prepend($('<img>',{id:'#key-pressed',src:'img/konami/letter-a.png'}));
                 bucketArray.push("a");
                 break;
             case 13:
-                $("#key-pressed").html("enter").prepend($('<img>',{id:'#key-pressed',src:'img/konami/return-arrow.png'}));
+                $("#key-pressed").html("").prepend($('<img>',{id:'#key-pressed',src:'img/konami/return-arrow.png'}));
                 bucketArray.push("enter");
                 break;
             default:
-                $("#key-pressed").html("This is not a valid input!");
+                $("#key-pressed").html("This is NOT valid input!");
         }
     //stores valid keys pressed by user in an array and also displays the stored keystrokes on the browser page using JQuery
-        $("#keys-captured").html(bucketArray);
-
+        $("#keys-captured").html(bucketArray + '  ');
 
     //will compare the keys pressed by user with the actual Konami code.  If the code entered by user matches the correct Konami code, then alert will launch.
         if (bucketArray.length === konamiCodeArray.length) {
