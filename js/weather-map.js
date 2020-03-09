a7501cb10cafcaddfcee9326e8f979418a54d70e
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-03-09 14:21:43 -0500

    added icons to weather map

--- js/weather-map.js
+++ js/weather-map.js
@@ -5,7 +5,6 @@ mapboxgl.accessToken = mapboxToken;
 // initialize load of map to San Antonio
 getWeather(29.433468,-98.499370);
 
-
 //Takes in the longitude/latitude values manually submitted by end user
 $('#long-lat-btn').click(function (event) { //id for the submit button which captures the latitude and longitude input
     event.preventDefault();
@@ -27,27 +26,30 @@ function getWeather(lati, longi) {
 
         // Display the current "real time" temperature information to the weather-details div
         $('#weather-right-now').html('<p>' + "Current Weather:  " + data.currently.summary + '<br>' +
-            "Temperature:  " + data.currently.temperature + "  " +
+            "<img src='" + weatherIcon(iconArray, data.currently.icon) + "'>" + '<br>' +
+            "Current Temperature:  " + data.currently.temperature.toFixed(0) + '&deg' + '<br>' +
             "Humidity:  " + data.currently.humidity + "  " +
             "Pressure:  " + data.currently.pressure + '<br>' +
 
             "Current Time: " + new Date(data.currently.time * 1000) + '</p>');
 
-        // Display the current city by timezone label to the first h2 in body
+        // Display the current city by timezone label to the first h2 on page
         $('.city').html("Current weather in " + data.timezone);
 
         for (var i=0; i <= 2; i++) {
             displayForecast += '<div class="card" >';
+            // displayForecast += '<img class="card-img-top" src=' + weatherIcon(iconArray, data.daily.data[i].icon) + '>';
             displayForecast += "Summary:  " + data.daily.data[i].summary + '<br>';
-            displayForecast += "Temperature Low:  " + data.daily.data[i].temperatureLow + " High: " + data.daily.data[i].temperatureHigh + '<br>';
+            displayForecast += "<img src='" + weatherIcon(iconArray, data.daily.data[i].icon) + "'>";
+            displayForecast += "Temperature Low:  " + data.daily.data[i].temperatureLow.toFixed(0) + '&deg' + "/" +  " High: " + data.daily.data[i].temperatureHigh.toFixed(0) + '&deg' + '<br>';
             displayForecast += "Humidity:  " + data.daily.data[i].humidity + '<br>';
             displayForecast += "Pressure:  " + data.daily.data[i].pressure;
             displayForecast += '</div>';
         }
-
+        //display forecast gathered from the loop above to page
         $('#weather-forecast').html(displayForecast)
 
-    }); //end of getWeather function
+    }); //end of ajax call
 
     var map = new mapboxgl.Map({
         container: 'map',
@@ -61,16 +63,51 @@ function getWeather(lati, longi) {
         draggable: true
     })
 
-        .setLngLat([longi, lati])
-        .addTo(map);
+    .setLngLat([longi, lati])
+    .addTo(map);
 
     function onDragEnd() {
         var lngLat = marker.getLngLat();
 
         //pass the coordinates from marker on map to getWeather function
         getWeather(lngLat.lat, lngLat.lng);
-
     }
     marker.on('dragend', onDragEnd);
-}
 
+    // icon array
+    var iconArray = [{
+            link: "img/weather-icons/static/day.svg",
+            name: "clear-day"
+        },
+        {
+            link: "img/weather-icons/static/night.svg",
+            name: "clear-night"
+        },
+        {
+            link: "img/weather-icons/static/rainy-6.svg",
+            name: "rain"
+        },
+        {
+            link: "img/weather-icons/static/cloudy.svg",
+            name: "cloudy"
+        },
+        {
+            link: "img/weather-icons/static/cloudy-day-3.svg",
+            name: "partly-cloudy-day"
+        },
+        {
+            link: "img/weather-icons/static/cloudy-night-3.svg",
+            name: "party-cloudy-night"
+        }
+        ];
+
+    // function to change icons
+    function weatherIcon(icon, apiIcon) {
+        for (var i = 0; i < iconArray.length; i++) {
+            if (iconArray[i].name === apiIcon) {
+                return iconArray[i].link
+            }
+        }
+    }
+
+}
